dec4d9280b3f7333a9f3bb1b4093595777761490
chenste-osu <chen.st.we@gmail.com>
2023-02-24 16:57:49 -0500

    add functionality for zeroes and delete/AC

--- scripts.js
+++ scripts.js
@@ -1,33 +1,98 @@
 var memory = {
-    currentNum: "0",
+    currentNum: ["0"],
     operators: [],
-    isFull: false,
+    prevNums: []
 };
 
-function main() {
-    initButtons();
-    
-    document.getElementById("display").textContent = "0";
+function isNumEmpty() {
+    if (memory.currentNum.length == 1 && memory.currentNum[0] == "0") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function isFull(current) {
-    if (current.length > 9) {
+function isNumFull(digits) {
+    if (memory.currentNum.length > digits) {
         return true;
     } 
     return false;
 }
 
+function isOpEmpty() {
+    if (memory.operators.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function firstZeroPop() {
+    if (memory.currentNum.length == 1 && isNumEmpty()) {
+        memory.currentNum.pop();
+    }
+}
+
+function updateDisplay(num) {
+    let display = document.getElementById("display");
+    display.textContent = num;
+}
+
 function clickNum(num) {
-    if (isFull(memory.currentNum)) {
+    if (isNumFull(9)) {
         return
     }
-    if (memory.currentNum == "0") {
-        memory.currentNum = num;
-    } else {
-        memory.currentNum += num;
+    firstZeroPop();
+    memory.currentNum.push(num);
+    updateDisplay(memory.currentNum.join(""));
+}
+
+function clickZero(digits) {
+    if (isNumEmpty()) {
+        return;
+    } 
+    else if (digits == 1 && isNumFull(9)) {
+        return;
+    } 
+    else if(digits == 2 && isNumFull(8)) {
+        return;
     }
-    document.getElementById("display").textContent = memory.currentNum;
+    else {
+        for (let i = 0; i < digits; i++) {
+            memory.currentNum.push("0");
+        }
+    }
+    updateDisplay(memory.currentNum.join(""));
+}
+
+function deleteNum() {
+    if (isNumEmpty()) {
+        return;
+    }
+    if (memory.currentNum.length > 0) {
+        memory.currentNum.pop();
+    }
+    if (memory.currentNum.length < 1) {
+        memory.currentNum.push("0");
+    }
+    updateDisplay(memory.currentNum.join(""));
+}
+
+function allClear() {
+    if (!isNumEmpty()) {
+        console.log("current length", memory.currentNum.length);
+        for (let n = 0; n <= memory.currentNum.length; n++) {
+            memory.currentNum.pop();
+        }
+        memory.currentNum.push("0");
+    }
     console.log(memory.currentNum);
+    if (!isOpEmpty()) {
+        for (let o = 0; o < memory.operators.length + 1; o++) {
+            memory.operators.pop();
+        }
+    }
+    updateDisplay(memory.currentNum.join(""));
 }
 
 function initButtons() {
@@ -40,6 +105,20 @@ function initButtons() {
     initSeven();
     initEight();
     initNine();
+    initZero();
+    initDoubleZero();
+    initDelete();
+    initAllClear();
+}
+
+function initZero() {
+    let zero = document.getElementsByClassName("zero");
+    zero[0].addEventListener("click", () => clickZero(1));
+}
+
+function initDoubleZero() {
+    let doublezero = document.getElementsByClassName("doublezero");
+    doublezero[0].addEventListener("click", () => clickZero(2));
 }
 
 function initOne() {
@@ -85,6 +164,20 @@ function initEight() {
 function initNine() {
     let nine = document.getElementsByClassName("nine");
     nine[0].addEventListener("click", () => clickNum("9"));
+}
+
+function initDelete() {
+    let del = document.getElementsByClassName("delete");
+    del[0].addEventListener("click", () => deleteNum());
+}
+
+function initAllClear() {
+    let clear = document.getElementsByClassName("clear");
+    clear[0].addEventListener("click", () => allClear());
+}
+
+function main() {
+    initButtons();
 }
 
 main();
