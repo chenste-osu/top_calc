f469dd15fd8f2cd80d48bcf02db1da0f8980b45c
chenste-osu <chen.st.we@gmail.com>
2023-02-28 23:32:38 -0500

    add functionality for addition

--- scripts.js
+++ scripts.js
@@ -1,11 +1,54 @@
 var memory = {
     currentNum: ["0"],
-    operators: [],
-    prevNums: []
+    operator: [],
+    prevNum: "0",
+    operatorPressed: false
 };
+
+function setOpPress(binary) {
+    memory.operatorPressed = binary;
+}
+
+function parseCurrent() {
+    // check and convert number to a float
+    let curr = memory.currentNum.join("");
+    curr = parseFloat(curr);
+    if (!isNaN(curr)) {
+        return curr;
+    }
+}
+
+function parsePrev() {
+    let prev = parseFloat(memory.prevNum);
+    if (!isNaN(prev)) {
+        return prev;
+    }
+}
+
+function parseOp() {
+    if (!isOpEmpty()) {
+        return memory.operator[0];
+    }
+}
 
 function isNumEmpty() {
     if (memory.currentNum.length == 1 && memory.currentNum[0] == "0") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isPrevEmpty() {
+    if (memory.prevNum == "0") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isOpEmpty() {
+    if (memory.operator.length == 0) {
         return true;
     } else {
         return false;
@@ -19,12 +62,31 @@ function isNumFull(digits) {
     return false;
 }
 
-function isOpEmpty() {
-    if (memory.operators.length > 0) {
-        return true;
-    } else {
-        return false;
+function storeNum(num) {
+    memory.prevNum = num;
+}
+
+function storeOp(op) {
+    clearOp();
+    memory.operator.push(op);
+}
+
+function delOp() {
+    memory.operator.pop();
+}
+
+function clearNum() {
+    memory.currentNum.length = 0;
+}
+
+function clearOp() {
+    if (!isOpEmpty()) {
+        memory.operator.pop();
     }
+}
+
+function clearPrev() {
+    memory.prevNum = "0";
 }
 
 function firstZeroPop() {
@@ -34,14 +96,74 @@ function firstZeroPop() {
 }
 
 function updateDisplay(num) {
+    if (num.length > 10) {
+        num = "OVERFLOW";
+    }
     let display = document.getElementById("display");
     display.textContent = num;
+}
+
+function operate() {
+    let prevnum = parsePrev();
+    let currentnum = parseCurrent();
+    let operator = parseOp();
+    let result = "";
+
+    console.log("operate activated")
+    console.log("prevnum", prevnum);
+    console.log("currentnum", currentnum);
+    console.log("operator", operator);
+
+    if (operator == "+") {
+        result = prevnum + currentnum;
+    } else if (operator == "-") {
+        result = prevnum - currentnum;
+    } else if (operator == "*") {
+        result = prevnum * currentnum;
+    } else if (operator == "/") {
+        result == prevnum / currentnum;
+    }
+
+    console.log("result", result);
+
+    updateDisplay(result);
+    storeNum(result);
+
+    // update display needs a way to overwrite a prevnum on display
+
+    clearNum();
+
+}
+
+function clickAdd() {
+    // first need to operate on the prev 2 nums
+    // then set the current operator which can change when pressing another operator
+    // pressing equals simply evaluates the 2 nums and operator and updates display
+    if (memory.operatorPressed == true) {
+        storeOp("+");
+        return;
+    }
+    storeOp("+");
+    operate();
+}
+
+function clickSubtract() {
+    //memory.operators.push("-");
+}
+
+function clickMultiply() {
+    //memory.operators.push("*");
+}
+
+function clickDivide() {
+    //memory.operators.push("/");
 }
 
 function clickNum(num) {
     if (isNumFull(9)) {
         return
     }
+    setOpPress(false);
     firstZeroPop();
     memory.currentNum.push(num);
     updateDisplay(memory.currentNum.join(""));
@@ -79,19 +201,11 @@ function deleteNum() {
 }
 
 function allClear() {
-    if (!isNumEmpty()) {
-        console.log("current length", memory.currentNum.length);
-        for (let n = 0; n <= memory.currentNum.length; n++) {
-            memory.currentNum.pop();
-        }
-        memory.currentNum.push("0");
-    }
-    console.log(memory.currentNum);
-    if (!isOpEmpty()) {
-        for (let o = 0; o < memory.operators.length + 1; o++) {
-            memory.operators.pop();
-        }
-    }
+    clearNum();
+    memory.currentNum.push("0");
+    clearOp();
+    clearPrev();
+    
     updateDisplay(memory.currentNum.join(""));
 }
 
@@ -109,6 +223,7 @@ function initButtons() {
     initDoubleZero();
     initDelete();
     initAllClear();
+    initAdd();
 }
 
 function initZero() {
@@ -174,6 +289,16 @@ function initDelete() {
 function initAllClear() {
     let clear = document.getElementsByClassName("clear");
     clear[0].addEventListener("click", () => allClear());
+}
+
+function initOperate() {
+    let operate = document.getElementsByClassName("operate");
+    operate[0].addEventListener("click", () => operate());
+}
+
+function initAdd() {
+    let add = document.getElementsByClassName("add");
+    add[0].addEventListener("click", () => clickAdd());
 }
 
 function main() {
